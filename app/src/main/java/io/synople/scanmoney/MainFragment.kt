61c3e0c0ef66ca31c5f96696c5b89e71f79f7c18
Jason Tu <title.kubo@gmail.com>
2018-10-13 07:43:58 -0400

    Now displays the breakdown

--- app/src/main/java/io/synople/scanmoney/MainFragment.kt
+++ app/src/main/java/io/synople/scanmoney/MainFragment.kt
@@ -7,20 +7,30 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import com.google.ar.core.Anchor
 import com.google.ar.core.Config
+import com.google.ar.core.TrackingState
 import com.google.ar.sceneform.AnchorNode
 import com.google.ar.sceneform.rendering.ViewRenderable
 import com.google.ar.sceneform.ux.ArFragment
 import com.google.ar.sceneform.ux.TransformableNode
 import kotlinx.android.synthetic.main.fragment_ar.*
+import java.lang.IllegalStateException
 import kotlin.concurrent.thread
 
 
 class MainFragment : Fragment() {
 
-    lateinit var recognizer: Recognizer
-    lateinit var arFragment: ArFragment
-
+    private lateinit var recognizer: Recognizer
+    private lateinit var arFragment: ArFragment
+    private val anchors: MutableList<Anchor> = mutableListOf()
+    private var totalCount = 0
+    private var washingtons = 0
+    private var lincolns = 0
+    private var hamiltons = 0
+    private var jacksons = 0
+    private var grants = 0
+    private var franklins = 0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -31,60 +41,20 @@ class MainFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        var totalCount = 0
         view.findViewById<TextView>(R.id.total).text = totalCount.toString()
         recognizer = Recognizer(context)
 
         scan.setOnClickListener {
-            val image =
-                (childFragmentManager.findFragmentById(R.id.uxFragment) as ArFragment).arSceneView.arFrame.acquireCameraImage()
+            scan.text = "Scanning..."
+            scan.isEnabled = false
 
             thread(true) {
-                val moneyList = recognizer.getFaces(image)
-
-                activity!!.runOnUiThread {
-                    moneyList.forEach { money ->
-                        val anchor = arFragment.arSceneView.arFrame.hitTest(money.x, money.y)[0].createAnchor()
-
-                        ViewRenderable.builder()
-                            .setView(context, R.layout.renderable_money)
-                            .build()
-                            .thenAccept { renderable ->
-                                val anchorNode = AnchorNode(anchor)
-                                anchorNode.setParent(arFragment.arSceneView.scene)
-
-                                when (money.value) {
-                                    "washington" -> (renderable?.view as TextView).text = "$1"
-                                    "lincoln" -> (renderable?.view as TextView).text = "$5"
-                                    "hamilton" -> (renderable?.view as TextView).text = "$10"
-                                    "jackson" -> (renderable?.view as TextView).text = "$20"
-                                    "grant" -> (renderable?.view as TextView).text = "$50"
-                                    "franklin" -> (renderable?.view as TextView).text = "$100"
-                                }
-                                val transformableNode = TransformableNode(arFragment.transformationSystem)
-                                transformableNode.setParent(anchorNode)
-                                transformableNode.renderable = renderable
-                            }
-
-                        when (money.value) {
-                            "washington" -> totalCount += 1
-                            "lincoln" -> totalCount += 5
-                            "hamilton" -> totalCount += 10
-                            "jackson" -> totalCount += 20
-                            "grant" -> totalCount += 50
-                            "franklin" -> totalCount += 100
-                        }
-
-                        if (totalCount > 0) {
-                            total.text = "Total: $" + totalCount.toString()
-                            total.visibility = View.VISIBLE
-                        }
-                    }
-                }
+                analyze()
             }
         }
 
         arFragment = (childFragmentManager.findFragmentById(R.id.uxFragment) as ArFragment)
+        arFragment.arSceneView.planeRenderer.isVisible = false
         arFragment.setOnTapArPlaneListener { hitResult, plane, motionEvent ->
             val anchor = hitResult.createAnchor()
 
@@ -107,15 +77,134 @@ class MainFragment : Fragment() {
             config.focusMode = Config.FocusMode.AUTO
             config.updateMode = Config.UpdateMode.LATEST_CAMERA_IMAGE
             arFragment.arSceneView.session.configure(config)
+            autoFocus.visibility = View.GONE
         }
 
-        thread(false) {
-            Thread.sleep(3000)
-            val image =
-                (childFragmentManager.findFragmentById(R.id.uxFragment) as ArFragment).arSceneView.arFrame.acquireCameraImage()
+        total.setOnClickListener {
+            total.visibility = View.INVISIBLE
+            breakdown.visibility = View.VISIBLE
+        }
 
-            val money = recognizer.getFaces(image)
-            Log.v("Money", money.toString())
+        breakdown.setOnClickListener {
+            total.visibility = View.VISIBLE
+            breakdown.visibility = View.INVISIBLE
+        }
+
+        thread(true) {
+            while (true) {
+                Thread.sleep(3000)
+                try {
+                    analyze()
+                } catch (e: IllegalStateException) {
+                }
+            }
+        }
+    }
+
+    private fun analyze() {
+        val image =
+            (childFragmentManager.findFragmentById(R.id.uxFragment) as ArFragment).arSceneView.arFrame.acquireCameraImage()
+
+        val moneyList = recognizer.getFaces(image)
+        image.close()
+
+        totalCount = 0
+        washingtons = 0
+        lincolns = 0
+        hamiltons = 0
+        jacksons = 0
+        grants = 0
+        franklins = 0
+        anchors.forEach {
+            it.detach()
+        }
+        anchors.clear()
+
+        activity!!.runOnUiThread {
+            moneyList.forEach { money ->
+                if (arFragment.arSceneView.arFrame.hitTest(money.x, money.y).size == 0) {
+                    return@forEach
+                }
+
+                val anchor = arFragment.arSceneView.arFrame.hitTest(money.x, money.y)[0].createAnchor()
+
+                anchors.add(anchor)
+
+                ViewRenderable.builder()
+                    .setView(context, R.layout.renderable_money)
+                    .build()
+                    .thenAccept { renderable ->
+                        val anchorNode = AnchorNode(anchor)
+                        anchorNode.setParent(arFragment.arSceneView.scene)
+
+                        when (money.value) {
+                            "washington" -> (renderable?.view as TextView).text = "$1"
+                            "lincoln" -> (renderable?.view as TextView).text = "$5"
+                            "hamilton" -> (renderable?.view as TextView).text = "$10"
+                            "jackson" -> (renderable?.view as TextView).text = "$20"
+                            "grant" -> (renderable?.view as TextView).text = "$50"
+                            "franklin" -> (renderable?.view as TextView).text = "$100"
+                        }
+                        val transformableNode = TransformableNode(arFragment.transformationSystem)
+                        transformableNode.setParent(anchorNode)
+                        transformableNode.renderable = renderable
+                    }
+
+                when (money.value) {
+                    "washington" -> {
+                        totalCount += 1
+                        washingtons += 1
+                    }
+                    "lincoln" -> {
+                        totalCount += 5
+                        lincolns += 1
+                    }
+                    "hamilton" -> {
+                        totalCount += 10
+                        hamiltons += 1
+                    }
+                    "jackson" -> {
+                        totalCount += 20
+                        jacksons += 1
+                    }
+                    "grant" -> {
+                        totalCount += 50
+                        grants += 1
+                    }
+                    "franklin" -> {
+                        totalCount += 100
+                        franklins += 1
+                    }
+                }
+
+                if (totalCount > 0) {
+                    total.text = "Total: $" + totalCount.toString()
+                }
+
+                var breakdownString = ""
+                if (washingtons != 0) {
+                    breakdownString += "$1 - $washingtons\n"
+                }
+                if (lincolns != 0) {
+                    breakdownString += "$5 - $lincolns\n"
+                }
+                if (hamiltons != 0) {
+                    breakdownString += "$10 - $hamiltons\n"
+                }
+                if (jacksons != 0) {
+                    breakdownString += "$20 - $jacksons\n"
+                }
+                if (grants != 0) {
+                    breakdownString += "$50 - $grants\n"
+                }
+                if (franklins != 0) {
+                    breakdownString += "$100 - $franklins"
+                }
+                breakdown.text = breakdownString
+
+                scan.text = "Scan"
+                scan.isEnabled = true
+            }
         }
     }
 
